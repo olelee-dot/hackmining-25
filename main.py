8e0e7cd44ceeba189bc4116d35a8e4b1c83f02e8
olelee <olelee.me@gmail.com>
2025-01-11 11:41:46 +0100

    Create classes for intervall and bin parameters.

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+class Peak_To_Peak: 
+    initial_fill_height
+    initial_feeder_speed
+    timestamp_first_peak
+    minimum_fill_height
+    seconds_to_green
+    seconds_to_final_peak
+    final_fill_height
+
+class Bin_Parameters: 
+    warning_minimum_height
+    warning_maximum_height
+    emergency_stop_minimum_height
+    emergency_stop_maximum_height
